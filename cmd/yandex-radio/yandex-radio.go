ef0448e8a0baa5913855733a9879a56a358d57c6
Maxim Kovrov <maksim.kovrov@gmail.com>
2020-12-28 19:09:27 +0300

    Add notification

--- cmd/yandex-radio/yandex-radio.go
+++ cmd/yandex-radio/yandex-radio.go
@@ -4,20 +4,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/TheCreeper/go-notify"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 	"github.com/zserge/lorca"
 )
 
+// "github.com/mqu/go-notify"
+// 	"github.com/TheCreeper/go-notify"
+
+const (
+	notifyDelay       = int32(5000) // mills
+	songInfoFrequency = time.Second
+)
+
 func main() {
+	logrus.SetLevel(logrus.TraceLevel)
 	if err := doMain(); err != nil {
 		log.Fatal(err)
 	}
 }
-
-const songInfoFrequency = time.Second
 
 func doMain() error {
 	hd, err := homedir.Dir()
@@ -26,9 +35,9 @@ func doMain() error {
 		hd = "."
 	}
 
-	ui, err := lorca.New("https://radio.yandex.ru/", hd+"/.yandex-audio", 800, 600)
+	ui, err := lorca.New("https://radio.yandex.ru/", hd+"/.yandex-radio", 800, 600)
 	if err != nil {
-		log.Fatal(err)
+		logrus.WithError(err).Fatal("Fatal error")
 	}
 	defer ui.Close()
 
@@ -49,12 +58,17 @@ func doMain() error {
 	// }()
 
 	go func() {
-		songInfoFile := hd + "/.current-song"
+		var (
+			playingNow   = ""
+			songInfoFile = hd + "/.current-song"
+			songImgFile  = hd + "/.current-song-img.png"
+		)
 		_ = songInfoFile
 		ticker := time.Tick(songInfoFrequency)
 		for range ticker {
 			tt := ui.Eval(`document.querySelector('.player-controls__title').title`)
 			ta := ui.Eval(`document.querySelector('.player-controls__artists').title`)
+			ti := ui.Eval(`document.querySelector('.slider__item_playing div span').style.backgroundImage`)
 
 			trackInfo := fmt.Sprintf("%s • %s", tt, ta)
 			err := ioutil.WriteFile(songInfoFile, []byte(trackInfo), 0o664)
@@ -65,7 +79,43 @@ func doMain() error {
 				"tt":        tt,
 				"ta":        ta,
 				"trackInfo": trackInfo,
+				"ti":        ti,
 			}).Debug("Track info was got.")
+			if playingNow != "" && playingNow != trackInfo {
+				//https://developer.gnome.org/notification-spec/
+				ntf := notify.NewNotification("Yandex Radio", trackInfo)
+				ntf.Timeout = notifyDelay
+				func() {
+					if ti != nil && len(ti.String()) > 7 {
+
+						tiUrl := ti.String()[5 : len(ti.String())-2]
+						res, e2 := http.Get("http:" + tiUrl)
+						if e2 != nil {
+							logrus.WithError(e2).Warn("Unable to get song img")
+						}
+						defer res.Body.Close()
+						bb, e2 := ioutil.ReadAll(res.Body)
+						if e2 != nil {
+							logrus.WithError(e2).Warn("Unable to get song img")
+						}
+
+						e2 = ioutil.WriteFile(songImgFile, bb, 0o644)
+						if e2 != nil {
+							logrus.WithError(e2).Warn("Unable to save song img")
+						}
+
+						logrus.Tracef("tiUrl='%s'", tiUrl)
+						ntf.Hints = make(map[string]interface{})
+						ntf.Hints[notify.HintImagePath] = songImgFile
+					}
+				}()
+				_, err = ntf.Show()
+				if err != nil {
+					logrus.WithError(err).Warn("Unable to show notification")
+				}
+
+			}
+			playingNow = trackInfo
 		}
 	}()
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cured-plumbum/yandex-radio
 go 1.15
 
 require (
+	github.com/TheCreeper/go-notify v0.2.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/zserge/lorca v0.1.9
